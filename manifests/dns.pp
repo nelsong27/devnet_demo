bb40afecddb1fe699923165d16391bdb389381be
nelsong27 <nelson.gomez@puppet.com>
2020-10-21 13:19:56 -0400

    Update dns.pp

--- manifests/dns.pp
+++ manifests/dns.pp
@@ -3,7 +3,7 @@ ios_network_dns { 'settings':
   name   => 'settings',
   ensure => 'present',
   domain => 'puppet.com',
-  search => 'puppet.com',
+  search => [ 'puppet.com', 'other.com' ],
   ip_domain_lookup => false,
   servers => [ 'serverone', 'servertwo', 'serverthree' ]
  }
