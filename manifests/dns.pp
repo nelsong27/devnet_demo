4171b52b81bf99454047a88c49e94b6ce230b1cd
nelsong27 <nelson.gomez@puppet.com>
2020-10-21 13:02:09 -0400

    Update dns.pp

--- manifests/dns.pp
+++ manifests/dns.pp
@@ -1,9 +1,9 @@
 class devnet_demo::dns {
-iso_network_dns {
+iso_network_dns { 'settings':
   ensure => 'present',
   domain => 'puppet.com',
   search => 'puppet.com',
   servers => 'dns_server1.puppet.com',
-  namevar => 'settings',
-}
+ }
+ 
 }
