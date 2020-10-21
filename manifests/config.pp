0c211a7180325a1e6b93e069ad32b0858233fb35
nelsong27 <nelson.gomez@puppet.com>
2020-10-21 16:06:43 -0400

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,6 @@
 class devnet_demo::config {
 ios_config { 'nameserver':
-       command => 'ip name-server 255.255.255.255',
-       idempotent_regex => 'ip name-server 255.255.255.255'
+       command => 'ip name-server 1.1.1.1',
+       idempotent_regex => 'ip name-server 1.1.1.1'
      }
  }
